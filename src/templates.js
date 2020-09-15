d6c3d3e3a1286762c377d23bd156554c0629c4bf
Djabrail Gadjimuradov <info@djabrail.ru>
2020-09-15 15:43:10 +0400

    add templates

---
+++ src/templates.js
@@ -0,0 +1,30 @@
+function title(block) {
+  return `
+    <div class="row">
+      <div class="col-sm">
+          <h1>${block.value}</h1>
+      </div>
+    </div>`;
+}
+
+function text(block) {
+  return `
+    <div class="row">
+      <div class="col-sm">
+          <p>${block.value}</p>
+      </div>
+    </div>`;
+}
+
+function textColumns(block) {
+  const html = block.value.map(item => {
+    return `
+        <div class="col-sm">
+          <p>${item}</p>
+        </div>`;
+  });
+
+  return `<div class="row">${html.join("")}</div>`;
+}
+
+export const templates = { title, text, textColumns };
