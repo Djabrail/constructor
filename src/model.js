3f756255ad22e854b39f5332578ea78298006b6e
Djabrail Gadjimuradov <info@djabrail.ru>
2020-09-15 15:03:34 +0400

    add model

---
+++ src/model.js
@@ -0,0 +1,17 @@
+export const model = [
+  {
+    type: "title",
+    value: "Test Title",
+    options: {}
+  },
+  {
+    type: "text",
+    value: "Lorem ipsum dolor sit, amet consectetur adipisicing elit.",
+    options: {}
+  },
+  {
+    type: "textColumns",
+    value: ["1 title", "2 title", "3 title"],
+    options: {}
+  }
+];
